c8431919e3d99bc4b384404e17acaf9250a0eac8
miruuunaa <miruna.mitri@stud.ubbcluj.ro>
2024-10-14 11:36:47 +0300

    aufgabe1

---
+++ src/Electronik.java
@@ -0,0 +1,51 @@
+class Electronik {
+
+    public static int billig(int[] tastatur) {
+        int minPreis = tastatur[0];
+        for (int preis : tastatur) {
+            if (preis < minPreis) {
+                minPreis = preis;
+            }
+        }
+        return minPreis;
+    }
+
+    public static int teureste(int[] tastatur, int[] usbDrives) {
+        int maxPreis = 0;
+        for (int preis : tastatur) {
+            if (preis > maxPreis) {
+                maxPreis = preis;
+            }
+        }
+        for (int preis : usbDrives) {
+            if (preis > maxPreis) {
+                maxPreis = preis;
+            }
+        }
+        return maxPreis;
+    }
+
+    public static int teuersteUSBDrive(int[] usbDrives, int budget) {
+        int max = -1;
+        for (int preis : usbDrives) {
+            if (preis <= budget && preis > max) {
+                max = preis;
+            }
+        }
+        return max;
+    }
+
+    public static int maxBudget(int budget, int[] tastatur, int[] usbDrives) {
+        int max = -1;
+
+        for (int tastaturPreis : tastatur) {
+            for (int usbPreis : usbDrives) {
+                int total = tastaturPreis + usbPreis;
+                if (total <= budget && total > max) {
+                    max = total;
+                }
+            }
+        }
+        return max;
+    }
+}
