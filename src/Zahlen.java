3518edee65d525753597254de5aaa064a1f15e4d
miruuunaa <miruna.mitri@stud.ubbcluj.ro>
2024-10-15 22:52:42 +0300

    aufg3

--- src/Zahlen.java
+++ src/Zahlen.java
@@ -1,19 +1,17 @@
 //aufg3
-class Zahlen {
 
+class Zahlen {
     public static int[] summe(int[] num1, int[] num2) {
         int[] result = new int[num1.length + 1];
         int carry = 0;
-
         for (int i = num1.length - 1; i >= 0; i--) {
-            int sum = num1[i] + num2[i] + carry;
-            result[i + 1] = sum % 10;
-            carry = sum / 10;
+            int sum = num1[i] + num2[i] + carry;//suma cifrei curente
+            result[i + 1] = sum % 10;//cifra unitatilor
+            carry = sum / 10;//cifra zecilor
         }
+        result[0] = carry;//prima poz
 
-        result[0] = carry;
-
-        if (result[0] == 0) {
+        if (result[0] == 0) { //suma mai mica de 10
             int[] newResult = new int[result.length - 1];
             for (int i = 1; i < result.length; i++) {
                 newResult[i - 1] = result[i];
@@ -26,9 +24,8 @@ class Zahlen {
     }
 
     public static int[] differenz(int[] num1, int[] num2) {
-        int[] result = new int[num1.length];
+        int[] result = new int[num1.length];//stocare
         int borrow = 0;
-
         for (int i = num1.length - 1; i >= 0; i--) {
             int diff = num1[i] - num2[i] - borrow;
             if (diff < 0) {
@@ -56,13 +53,12 @@ public static int[] multiplikation(int[] num, int digit) {
         result[i + 1] = product % 10;
         carry = product / 10;
     }
-
     result[0] = carry;
 
-    if (result[0] > 0) {
+    if (result[0] > 0) { //s a folosit carry
         int[] newResult = new int[result.length + 1];
         for (int i = 0; i < result.length; i++) {
-            newResult[i + 1] = result[i];
+            newResult[i + 1] = result[i];//pe poz 0 se pune carry
         }
         result = newResult;
     }
@@ -76,7 +72,7 @@ public static int[] multiplikation(int[] num, int digit) {
     for (int i = start; i < result.length; i++) {
             finalResult[i - start] = result[i];
     }
-    if(negativ&&finalResult.length>0){
+    if(negativ && finalResult.length>0){
         finalResult[0]=-finalResult[0];
     }
     return finalResult;
@@ -101,9 +97,9 @@ public static int[] multiplikation(int[] num, int digit) {
         if (start == result.length) {
             return new int[]{0};
         } else {
-            int[] newResult = new int[result.length - start];
+            int[] newResult = new int[result.length - start];//cate cifra sunt dupa primele 0
             for (int i = start; i < result.length; i++) {
-                newResult[i - start] = result[i];
+                newResult[i - start] = result[i];//sunt asezate fara 0 de la inceput
             }
             return newResult;
         }
